07b7172649712d067d2f67b2b0e2e75611c3c87f
Kyle Johnson <kyle@Kyles-MacBook-Pro.local>
2017-10-10 12:20:34 -0400

    touchup

--- frontend/app/js/component/App.jsx
+++ frontend/app/js/component/App.jsx
@@ -11,13 +11,14 @@ export default function App() {
       <div className="alert alert-primary" role="alert">
         This is a primary alert—check it out!
       </div>
-      <div className="top-bar">
-        <a src="" className="history-button" ><img src={c1Logo} alt="Home" /></a>
-        <HistoryBtn title={" Logo History"} logo={chipmunkLogo} />
-        <HistoryBtn title={" Credit Card Appoval History"} logo={creditCardLogo} />
-        <a src="" className="user-links">Help</a>
-        <a src="" className="user-links">Settings</a>
-        <a src="" className="user-links">Sign Out</a>
+      <div className="row top-bar">
+        <a src="" className="history-button col-md-1" ><img src={c1Logo} alt="Home" /></a>
+        <HistoryBtn title={" Logo History"} logo={chipmunkLogo} className="col-md-2" />
+        <HistoryBtn title={" Credit Card Appoval History"} logo={creditCardLogo} className="col-md-2" />
+        <div className="col-md-4"></div>
+        <a src="" className="col-md-1 user-links">Help</a>
+        <a src="" className="col-md-1 user-links">Settings</a>
+        <a src="" className="col-md-1 user-links">Sign Out</a>
       </div>
       <div>
         <MLNavbar child1={<div><p>Hello</p></div>} child2={<div><p>Goodbye</p></div>} />
