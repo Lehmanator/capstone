3018aa05660c0537c49ef54407284119c5fed779
Sushrut Shringarputale <sush.shring@gmail.com>
2017-10-10 21:08:52 -0400

    Added the upload image route

--- api/src/main/java/api/UploadLogoController.java
+++ api/src/main/java/api/UploadLogoController.java
@@ -1,24 +1,65 @@
 package api;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import javafx.concurrent.Task;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 /**
  * Created by sushrutshringarputale on 10/10/17.
  */
 @RestController
 public class UploadLogoController {
-    @RequestMapping(value = "/recognition", method = RequestMethod.POST)
-    public ApiResponse upload (
-            @RequestParam("image")MultipartFile image
-            ) {
-        
-        return null;
+
+    private static String recognitionAPIKey = "c2geZf8u9PeAGBiwTlw2hjaT0B6ZGz86";
+    private static String imageRecognitionUri = "http://ml-backend-dev.us-east-2.elasticbeanstalk.com/process_image";
+
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public UploadApiResponse upload(
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("name") String name,
+            @RequestParam("user") String username
+    ) {
+        try {
+            DBHandler dbHandler = new DBHandler(DBConnector.getInstance().getConnection(), DBConnector.getInstance().getAmazonS3());
+            if (!image.isEmpty()) {
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.addUserMetadata("name", image.getOriginalFilename());
+                String id = UUID.randomUUID().toString().replace("-", "");
+                metadata.addUserMetadata("id", id);
+
+                //Upload image to S3 bucket
+                String location = dbHandler.uploadImage(username, name, image.getInputStream(), metadata);
+                UploadApiResponse response = new UploadApiResponse(HttpStatus.OK, "Image successfully uploaded", location, id);
+                return response;
+            } else {
+                return new UploadApiResponse(HttpStatus.BAD_REQUEST, "Image must be provided");
+            }
+        } catch (IOException e) {
+            return new UploadApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Image could not be read");
+        } catch (SQLException e) {
+            return new UploadApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Image could not be uploaded");
+        }
     }
 
 }
--- api/src/main/java/api/Users.java
+++ api/src/main/java/api/Users.java
@@ -8,101 +8,101 @@ import java.util.Map;
 import java.util.UUID;
 
 public class Users {
-  private volatile static Users instance = null;
-  private static DBHandler handler;
+    private volatile static Users instance = null;
+    private static DBHandler handler;
 
-  private Map<String, User> users;
-  private Map<String, String> sessionKeys;
+    private Map<String, User> users;
+    private Map<String, String> sessionKeys;
 
-  private Users() throws SQLException {
-    users = new HashMap<>();
-    sessionKeys = new HashMap<>();
-    DBConnector connector = DBConnector.getInstance();
-    handler = new DBHandler(connector.getConnection(), connector.getAmazonS3());
-  }
+    private Users() throws SQLException {
+        users = new HashMap<>();
+        sessionKeys = new HashMap<>();
+        DBConnector connector = DBConnector.getInstance();
+        handler = new DBHandler(connector.getConnection(), connector.getAmazonS3());
+    }
 
-  public static Users getInstance() throws SQLException {
-    if (instance == null) {
-      synchronized (Users.class) {
+    public static Users getInstance() throws SQLException {
         if (instance == null) {
-          instance = new Users();
+            synchronized (Users.class) {
+                if (instance == null) {
+                    instance = new Users();
+                }
+            }
         }
-      }
+        return instance;
     }
-    return instance;
-  }
 
-  public Map<String, User> getAllUsers() {
-    return users;
-  }
-
-  public User getUser(String username) throws SQLException {
-    if (users.containsKey(username)) {
-      return users.get(username);
-    } else {
-      List<String> params = new ArrayList<>();
-      params.add("username");
-      params.add("password");
-      params.add("firstname");
-      params.add("lastname");
-      params.add("profpicurl");
-      params.add("id");
-      String query = handler.createSelectUsersTableQuery(params, "username", username);
-      List<Map<String, Object>> responses = handler.executeQueryWithResults(query);
-      for (Map<String, Object> response : responses) {
-        User curr = new User((String) response.get("username"),
-            (String) response.get("password"),
-            (String) response.get("firstname"),
-            (String) response.get("lastname"),
-            (String) response.get("profpicurl"),
-            (Integer) response.get("id"));
-        users.put((String) response.get("username"), curr);
-      }
+    public Map<String, User> getAllUsers() {
+        return users;
     }
-    return users.get(username);
-  }
 
-  public void changeUser(User user) {
-    users.put(user.username(), user);
-  }
-
-  public void save() throws SQLException {
-    for (User user : users.values()) {
-      Map<String, String> params = new HashMap<>();
-      params.put("password", user.password());
-      params.put("firstname", user.firstName());
-      params.put("lastname", user.lastName());
-      params.put("profilepic", user.profilePicUrl());
-      String query =
-          handler.createUpdateUsersTableQuery(params, "username", user.username());
-      handler.executeQuery(query);
+    public User getUser(String username) throws SQLException {
+        if (users.containsKey(username)) {
+            return users.get(username);
+        } else {
+            List<String> params = new ArrayList<>();
+            params.add("username");
+            params.add("password");
+            params.add("firstname");
+            params.add("lastname");
+            params.add("profpicurl");
+            params.add("id");
+            String query = handler.createSelectUsersTableQuery(params, "username", username);
+            List<Map<String, Object>> responses = handler.executeQueryWithResults(query);
+            for (Map<String, Object> response : responses) {
+                User curr = new User((String) response.get("username"),
+                        (String) response.get("password"),
+                        (String) response.get("firstname"),
+                        (String) response.get("lastname"),
+                        (String) response.get("profpicurl"),
+                        (Integer) response.get("id"));
+                users.put((String) response.get("username"), curr);
+            }
+        }
+        return users.get(username);
     }
-  }
 
-  public void addNewUser(
-      String username,
-      String password,
-      String firstname,
-      String lastname,
-      String profpicurl) throws SQLException {
-    Map<String, String> params = new HashMap<>();
-    params.put("username", username);
-    params.put("password", password);
-    params.put("firstname", firstname);
-    params.put("lastname", lastname);
-    params.put("profilepic", profpicurl);
-    String query = handler.createInsertUsersTableQuery(params);
-    handler.executeQuery(query);
-    getUser(username);
-  }
+    public void changeUser(User user) {
+        users.put(user.username(), user);
+    }
 
-  public String createSessionKey(String username) {
-    String key = UUID.randomUUID().toString().replace("-", "");
-    sessionKeys.put(username, key);
-    return key;
-  }
+    public void save() throws SQLException {
+        for (User user : users.values()) {
+            Map<String, String> params = new HashMap<>();
+            params.put("password", user.password());
+            params.put("firstname", user.firstName());
+            params.put("lastname", user.lastName());
+            params.put("profilepic", user.profilePicUrl());
+            String query =
+                    handler.createUpdateUsersTableQuery(params, "username", user.username());
+            handler.executeQuery(query);
+        }
+    }
 
-  public String getSessionKey(String username) {
-    return sessionKeys.get(username);
-  }
+    public void addNewUser(
+            String username,
+            String password,
+            String firstname,
+            String lastname,
+            String profpicurl) throws SQLException {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        params.put("firstname", firstname);
+        params.put("lastname", lastname);
+        params.put("profilepic", profpicurl);
+        String query = handler.createInsertUsersTableQuery(params);
+        handler.executeQuery(query);
+        getUser(username);
+    }
+
+    public String createSessionKey(String username) {
+        String key = UUID.randomUUID().toString().replace("-", "");
+        sessionKeys.put(username, key);
+        return key;
+    }
+
+    public String getSessionKey(String username) {
+        return sessionKeys.get(username);
+    }
 }
