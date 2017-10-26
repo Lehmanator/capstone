492e2e155b2ca7896a6456d581fb81209cbe4ff7
John Barczynski <jeb5875@psu.edu>
2017-10-25 23:13:06 -0400

    Creating History page for frontend

---
+++ frontend/app/js/component/History.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+export default function History() {
+  return (
+    <div>
+      <div>
+        <h2> Welcome to the history page </h2>
+      </div>
+    </div>
+  );
+}
