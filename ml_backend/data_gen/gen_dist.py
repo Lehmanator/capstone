7d890a61056d7f3a5f1c77d77b69b32d98c38772
Sushrut Shringarputale <sush.shring@gmail.com>
2017-10-30 23:17:42 -0400

    Added some data_gen capabilities. Need to debug

--- ml_backend/data_gen/gen_dist.py
+++ ml_backend/data_gen/gen_dist.py
@@ -1,30 +1,76 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sci
-import matplotlib as plt
+from scipy.interpolate import UnivariateSpline
 
+numfigs = 1
 
 class User:
-    def __init__(self, credit_data, id_data):
-        self.credit_data = credit_data
+    def __init__(self, c_data, id_data):
+        self.credit_data = c_data
         self.id_data = id_data
 
 
 class CreditData:
     # TODO gather list of variables
-    def __init__(self, var_dict):
+    def __init__(self, var_dict, name):
+        self.name = name
+        self.vars = {}
         for var in var_dict.keys():
-            self.vars = DistData(var, var_dict[var])
+            self.vars[var] = DistData(var + name, var_dict[var])
 
-    def gen_data(self):
-        pass
+    def plot(self):
+        num = numfigs
+        figs = []
+        for prop in self.vars:
+            num += 1
+            figs.append(self.vars[prop].plot(num))
+        return figs
 
 
 class DistData:
-    def __init__(self, name, var_data):
+    def __init__(self, name, v_data):
         self.name = name
-        self.dist = find_dist(var_data)
-        self.dist_params = find_dist_params(self.dist, var_data)
-        self.sample = gen_dist(self.dist_params)
+        self.var_data = v_data
+        self.dist = self.__find_dist__(v_data)
+
+    @staticmethod
+    def __find_dist__(var_data):
+        # Assuming that var_data is dict containing two keys: "yes" and "no", with each key containing the input data
+        # for credit score approvals and corresponding properties
+        # TODO Figure out how to do this.
+        yes_data = var_data["yes"]
+        no_data = var_data["no"]
+        dists = {"yes": [], "no": []}
+        p1, x1 = np.histogram(yes_data, bins=len(yes_data) // 10)
+        x1 = x1[:-1] + (x1[1] - x1[0]) / 2
+        norm_y1 = [float(a) / sum(p1) for a in p1]
+        dists["yes"] = [x1, norm_y1]
+        p2, x2 = np.histogram(no_data, bins=len(yes_data) // 10)
+        x2 = x2[:-1] + (x2[1] - x2[0]) / 2
+        norm_y2 = [float(a) / sum(p2) for a in p2]
+        dists["no"] = [x2, norm_y2]
+        return dists
+        pass
+
+    def plot(self, num):
+        f = UnivariateSpline(self.dist["yes"][0].tolist(), self.dist["yes"][1], s=10)
+        g = UnivariateSpline(self.dist["no"][0].tolist(), self.dist["no"][1], s=10)
+        x1 = self.dist["yes"][0]
+        x2 = self.dist["no"][0]
+        plt.plot(x1, f(x1), 'b', label="yes")
+        plt.plot(x2, g(x2), 'r', label="no")
+        plt.title(self.name)
+        plt.legend(loc="upper left")
+        plt.xlabel("Income value")
+        plt.ylabel("Frequency")
+        return plt.figure(num)
+        pass
+
+    def generate(self, count, type):
+        if type:
+            return np.random.choice(self.dist["yes"][0].tolist(), count, self.dist["yes"][1])
+        else:
+            return np.random.choice(self.dist["no"][0].tolist(), count, self.dist["no"][1])
 
 
 def mean(var_data):
@@ -38,31 +84,7 @@ def standard_dev(var_data):
     pass
 
 
-def find_dist(var_data):
-    # TODO Figure out how to do this.
-    sci.fit
-    pass
 
-
-def find_dist_params(dist_str, var_data):
-    if dist_str == 'normal':
-        mean = mean(var_data)
-        size = len(var_data)
-        std = standard_dev(var_data, mean)
-        return np.random.normal(mean, size, std)
-    elif dist_str == 'binomial':
-        pass
-    elif dist_str == 'exponential':
-        pass
-    elif dist_str == 'geometric':
-        pass
-    elif dist_str == 'hypergeometric':
-        pass
-    elif dist_str == 'lognormal':
-        pass
-    elif dist_str == 'poisson':
-        pass
-        # TODO more distributions
 
 
 def gen_dist(dist_str, params):
@@ -74,3 +96,26 @@ def gen_dist(dist_str, params):
 def histogram(var_data):
     count, bins, ignored = plt.hist(var_data, 50, normed=True)
     plt.show()
+    pass
+
+
+def read_data():
+    return {"income": {"yes": np.asarray(np.load("income_sample_yes"))[0].tolist(), "no": np.asarray(np.load("income_sample_no"))[0].tolist()}
+            # "age": {"yes": np.load("age_sample_yes"), "no": np.load("age_sample_no")},
+            # "scores": {"yes": np.load("scores_sample_yes"), "no": np.load("scores_sample_no")},
+            # "rent": {"yes": np.load("rent_sample_yes"), "no": np.load("rent_sample_no")}
+            }
+
+
+var_data = read_data()
+credit_data = CreditData(var_data, "orig")
+f = credit_data.plot()
+numfigs += len(f)
+
+gen_dict = {}
+for i in credit_data.vars:
+    gen_dict[i] = {"yes": credit_data.vars[i].generate(5000, True), "no": credit_data.vars[i].generate(5000, False)}
+    pass
+gen_data = CreditData(gen_dict, "gen")
+g = gen_data.plot()
+plt.show()
