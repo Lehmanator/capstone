bfa42fd616b835d18f71cfa10c0aa02be0a0bd08
Mayank Makwana <mayankmmmx@gmail.com>
2017-11-07 12:08:07 -0500

    rlly added it now

---
+++ api/src/main/java/api/db/applications/ApplicationsManager.java
@@ -0,0 +1,13 @@
+package api.db.applications;
+
+import api.db.applications.generated.GeneratedApplicationsManager;
+
+/**
+ * The main interface for the manager of every {@link
+ * api.db.applications.Applications} entity.
+ * <p>
+ * This file is safe to edit. It will not be overwritten by the code generator.
+ * 
+ * @author company
+ */
+public interface ApplicationsManager extends GeneratedApplicationsManager {}
