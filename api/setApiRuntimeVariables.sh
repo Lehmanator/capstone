60494be02003202d7619cc533c7462764ed2e9d4
John Barczynski <jeb5875@psu.edu>
2017-11-09 18:37:48 -0500

    Let's not put these here - also, they're wrong

--- api/setApiRuntimeVariables.sh
+++
@@ -1,5 +0,0 @@
-export RDS_DB_NAME_CAP="capitalfun"
-export RDS_USERNAME="capitalone"
-export RDS_PASSWORD="Capitalfun123!"
-export RDS_HOSTNAME="capitalfun.c21ogdmq7qho.us-east-2.rds.amazonaws.com"
-export RDS_PORT=3306
