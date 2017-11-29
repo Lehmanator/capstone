d3837ca4a348a7c5cf60d5bb68e48f20778f5abc
Mayank Makwana <mayankmmmx@gmail.com>
2017-11-28 20:22:36 -0500

    fixed the bug john found

--- api/src/main/java/api/controller/request/UploadApiRequest.java
+++ api/src/main/java/api/controller/request/UploadApiRequest.java
@@ -4,35 +4,19 @@ package api.controller.request;
  * Created by sushrutshringarputale on 10/12/17.
  */
 public class UploadApiRequest {
+  private String token;
   private String image;
   private String name;
-  private String token;
 
   public UploadApiRequest() {
-    image = "";
-    name = "";
-    token = "";
   }
 
-  public UploadApiRequest(String image, String name, String token) {
-    this.image = image;
-    this.name = name;
+  public UploadApiRequest(
+      String token,
+      String image,
+      String name) {
     this.token = token;
-  }
-
-  public String getImage() {
-    return image;
-  }
-
-  public void setImage(String image) {
     this.image = image;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
     this.name = name;
   }
 
@@ -40,7 +24,23 @@ public class UploadApiRequest {
     return token;
   }
 
-  public void setToken(String userId) {
+  public String getImage() {
+    return image;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setToken(String token) {
     this.token = token;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 }
