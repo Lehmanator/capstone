69e4638bd6976793086b0618eda3b5682bf85bfe
John Barczynski <jeb5875@psu.edu>
2017-11-30 11:33:12 -0500

    Fixing constants.jsx

--- frontend/app/js/component/constants.jsx
+++ frontend/app/js/component/constants.jsx
@@ -5,6 +5,7 @@ const constants = {
   uploadImageUrl: `${baseUrl}/upload`,
   historyUrl: `${baseUrl}/getHistory`,
   ccUploadData: `${baseUrl}/creditCheck`,
+  creditHistoryUrl: `${baseUrl}/getCreditCardHistory`,
   positivityThreshold: 0.69,
   defaultUser: 'mayank',
 };
