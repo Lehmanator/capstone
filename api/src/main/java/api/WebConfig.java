ec57ddb532056d0226f6b8f8e0e9686d5a7f9f80
Mayank Makwana <mayankmmmx@gmail.com>
2017-12-03 11:35:02 -0500

    added CORS support

--- api/src/main/java/api/WebConfig.java
+++ api/src/main/java/api/WebConfig.java
@@ -5,12 +5,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * Web config for cors
+ * @author mayank
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "OPTIONS")
+        .allowedOrigins("http://capitalone-ml.me")
+        .allowCredentials(false)
+        .maxAge(3600);
   }
 }
--- api/src/main/java/api/controller/GetLogoHistoryController.java
+++ api/src/main/java/api/controller/GetLogoHistoryController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.logging.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
