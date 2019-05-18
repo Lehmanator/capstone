9d5a6e6b119061d15bf2cb3e435610410e001e00
Sam Lehman <samlehman617@gmail.com>
2019-05-18 02:39:02 -0400

    Updated some deps, working on making deployable.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,51 +1,52 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/js/main'),
+  entry: path.resolve(__dirname, "app/js/main"),
   devServer: {
-    outputPath: path.join(__dirname, 'build'),
+    outputPath: path.join(__dirname, "build"),
+    historyApiFallback: true
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
-    path: path.resolve(__dirname, 'build/js'),
-    publicPath: '/js/',
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build/js"),
+    publicPath: "/js/",
+    filename: "bundle.js"
   },
   plugins: [
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(["build"]),
     new CopyWebpackPlugin([
       {
-        context: path.resolve(__dirname, 'app/static'),
-        from: '**/*',
-        to: path.resolve(__dirname, 'build'),
-      },
-    ]),
+        context: path.resolve(__dirname, "app/static"),
+        from: "**/*",
+        to: path.resolve(__dirname, "build")
+      }
+    ])
   ],
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: ['eslint'],
-      },
-    ],
+    //preLoaders: [
+    //  {
+    //    test: /\.jsx?$/,
+    //    exclude: /(node_modules|bower_components)/,
+    //    loaders: ['eslint'],
+    //  },
+    //],
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ['babel'],
+        loaders: ["babel-loader"]
       },
       {
         test: /\.(gif|png|jpg|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]',
-        },
-      },
-    ],
-  },
+          name: "[path][name].[ext]"
+        }
+      }
+    ]
+  }
 };
